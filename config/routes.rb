01298295589ed4ea8aca81847d37ccd7410ac172
Jared Ning <jared@redningja.com>
2014-01-25 21:08:23 -0600

    Remove old routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 OscarsMadness::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  # contest_year is prepended to paths.
-  #map.filter :contest_year, :file => "#{RAILS_ROOT}/lib/routing_filter_for_contest_year"
 
   root :controller => 'home', :action => 'index'
   get 'about', :controller => 'home', :action => 'about'
@@ -10,11 +8,8 @@ OscarsMadness::Application.routes.draw do
   get    'login'                => 'sessions#new', as: :login_form
   post   'create_session'       => 'sessions#create', as: :login
   get    'logout'               => 'sessions#destroy', as: :logout
-  get    'login_admin'          => 'sessions#new_admin', as: :login_admin_form
-  post   'create_session_admin' => 'sessions#create_admin', as: :login_admin
 
   resources :players do
-    resources :entries, only: [:create]
     resources :picks, only: [:index] do
       collection do
         get 'edit'
@@ -28,6 +23,4 @@ OscarsMadness::Application.routes.draw do
   # Omniauth
   get '/auth/failure'            => 'sessions#failure'
   get '/auth/:provider/callback' => 'sessions#create'
-
-  put '/admin/toggle_picks_editable' => 'admin#toggle_picks_editable', as: :toggle_picks_editable
 end
