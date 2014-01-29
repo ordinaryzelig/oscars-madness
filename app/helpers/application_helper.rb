2b07a5aea045febec5046e7a5816a453828ab29e
Jared Ning <jared@redningja.com>
2014-01-28 22:47:59 -0600

    Add standings styling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,39 @@ module ApplicationHelper
     @use_category_nav
   end
 
+  def avatar(player)
+    image_tag '/', class: 'ui image'
+  end
+
+  def progress_percentage(entry, categories)
+    ((entry.points.to_f / possible_points(categories)) * 100).round
+  end
+
+  def possible_points(categories)
+    categories.select(&:has_winner?).map(&:points).reduce(:+)
+  end
+
+  def standing_progress_color(entry, entries)
+    case
+    when top_scorer?(entry, entries)
+      'successful'
+    when bottom_scorer?(entry, entries)
+      'failed'
+    else
+      ''
+    end
+  end
+
+  def top_scorer?(entry, entries)
+    scores(entries).max == entry.points
+  end
+
+  def bottom_scorer?(entry, entries)
+    scores(entries).min == entry.points
+  end
+
+  def scores(entries)
+    entries.map(&:points)
+  end
+
 end
