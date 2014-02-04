c90086ae99a61c23b2429ef65672811255aa880c
Jared Ning <jared@redningja.com>
2014-02-04 00:45:58 -0600

    Add contest year select to bottom of categories nav.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
     !latest_contest_year?(contest_year)
   end
 
+  def contest_year_select
+    year_paths = Contest.years.map { |year| [year, root_path(:contest_year => year)] }
+    selected_year_path = root_path(:contest_year => contest_year)
+    select_options = options_for_select(year_paths, selected_year_path)
+
+    options = {
+      onchange: 'window.location = this.value',
+    }
+
+    select_tag :contest_year, select_options, options
+  end
+
 end
