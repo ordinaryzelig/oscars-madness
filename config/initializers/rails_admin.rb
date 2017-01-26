103053967b595d1fe4e835ed0a874d798e0f2fc9
Jared Ning <jared@redningja.com>
2017-01-25 21:59:29 -0600

    Set admin username/password for dev.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,9 +1,12 @@
 RailsAdmin.config do |config|
   config.main_app_name = ["Oscars Madness", "OscarsMadness"]
 
+  oscars_username = Rails.env.production? ? ENV.fetch('OSCARS_USERNAME') : 'admin'
+  oscars_password = Rails.env.production? ? ENV.fetch('OSCARS_PASSWORD') : 'admin'
+
   config.authorize_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == ENV.fetch('OSCARS_USERNAME') && password == ENV.fetch('OSCARS_PASSWORD')
+      username == oscars_username && password == oscars_password
     end
   end
 
